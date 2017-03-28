269ceae6596e4e9d67e79234c7bdc56b13ff3403
NevermindWhoIsGreen <artem.lipara@gmail.com>
2017-03-28 21:42:29 +0300

    +css +templates +logic for articles

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from blog import views
 
 urlpatterns = [
-   # url(r'^$', 'blog.views.home', name'home'),
+    url(r'^$', views.home , name='home'),
+    url(r'^about/$', views.about , name='about'),
+    url(r'^article/(?P<article_id>[0-9]+)/$', views.show_article , name='article'),
 ]
